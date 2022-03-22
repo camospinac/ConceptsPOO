e1c3a4f446501252a79ae05d8878ba191a04cead
Camilo Ospina <camospinac@outlook.com>
2022-03-22 14:50:47 -0500

    Date class finished!

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ConceptsPOO;
+
+Console.WriteLine("Conceptos POO");
+Console.WriteLine("=============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 28));
+    Console.WriteLine(new Date(2021, 8, 6));
+    Console.WriteLine(new Date(2020, 2, 29));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
